e7aa8e0c7f451a5c5579d1ea846f4977ac206674
kikuchi <natumetarou@gmail.com>
2026-05-14 10:55:45 +0900

    いいかんじ

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-window.addEventListener("scroll", function () {
-  // header要素を取得
-  const header = document.querySelector("header");
+// ヘッダーの縮小処理
+let scrolling = false;
 
-  // スクロール量が150pxを超えた場合
-  if (window.scrollY > 150) {
-    header.classList.add("shrink");
-  } else {
-    // 150px以下の場合
-    header.classList.remove("shrink");
+window.addEventListener("scroll", function () {
+  if (!scrolling) {
+    window.requestAnimationFrame(() => {
+      const header = document.querySelector("header");
+
+      if (window.scrollY > 150) {
+        header.classList.add("shrink");
+      } else {
+        header.classList.remove("shrink");
+      }
+      scrolling = false;
+    });
+    scrolling = true;
   }
 });
